c9eda9626932cb8c93ff0e8a9b30e277a551812a
mrglennc64 <mrglenncarter@gmail.com>
2026-05-21 21:59:14 +0200

    chore: update waitlist notify email to gmail

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
 export const CALENDLY_URL = "https://calendly.com/mrglenncarter/15min";
-export const WAITLIST_NOTIFY_EMAIL = "mrglenncarter@yahoo.com";
+export const WAITLIST_NOTIFY_EMAIL = "mrglenncarter@gmail.com";
